297b82902ffcac55106fa2340f9ef0bb35dc61b2
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-09 08:36:11 -0500

    Updated from Brython Server: 3/9/2018 8:36:10 AM

--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,31 @@
+#Matthew Suriawinata
+#3/9/18
+#rectangle.py - area and perimeter of rectangle
+
+def rectangle(a,b):
+    print("The area is", a*b)
+    print("The perimeter is", 2*a + 2*b)
+    
+rectangle(3,4)
+
+
+
+def printStars(num):
+    print("*" * num)
+    
+printStars(6)
+
+def countdown(num):
+    for i in range(0,num):
+        print(num - i)
+    print("BOOM!")
+    
+countdown(5)
+
+
+def vprint(word):
+    for ch in word:
+        print(ch)
+        
+vprint("Smeds")
 
