230077dd4fd1045b57851fe7cdac0d375066c233
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-22 08:08:10 -0400

    Updated from Brython Server: 3/22/2018 8:08:10 AM

--- bananaMonkey.py
+++ bananaMonkey.py
@@ -1,1 +1,15 @@
+#Matthew Suriawinata
+#3/22/18
+#monkey-banana.py - BEst game ever
 
+from ggmae import *
+if __name__ == "__main__":
+    
+    #colors
+    green = Color(0x006600,1)
+    
+    jungleBos = RectangleAsset(800,500,LineStyle(1,green),green)
+    
+    Sprite(jungleBos)
+    
+    App().run()
