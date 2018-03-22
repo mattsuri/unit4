8cc768d1b3c65a064a0dfeb420499c752397b911
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-22 08:15:13 -0400

    Updated from Brython Server: 3/22/2018 8:15:13 AM

--- bananaMonkey.py
+++ bananaMonkey.py
@@ -2,14 +2,26 @@
 #3/22/18
 #monkey-banana.py - BEst game ever
 
-from ggmae import *
+from ggame import *
+
+
+ROWS = 20
+COLS = 40
+CELL_SIZE = 20
+
+
 if __name__ == "__main__":
     
     #colors
     green = Color(0x006600,1)
+    brown = Color(0x8B4513,1)
     
-    jungleBos = RectangleAsset(800,500,LineStyle(1,green),green)
     
-    Sprite(jungleBos)
+    jungleBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(1,green),green)
+    monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(1,brown),brown)
+    
+    
+    Sprite(jungleBox)
+    Sprite(monkeyBox)
     
     App().run()
