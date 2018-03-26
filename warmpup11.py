4a2a7ffef08d6615511b829e33f6edd7f12aba2d
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-26 08:16:12 -0400

    Updated from Brython Server: 3/26/2018 8:16:09 AM

--- warmpup11.py
+++ warmpup11.py
@@ -1,1 +1,15 @@
+#Matthew Suriawinta
+#3/26/18
+#warmup11.py
 
+def(prime):
+    num = int(input("Enter a number: "))
+    while num != 1:
+        i += 1
+        if num % num-i == 0:
+            return(False)
+            break
+        else:
+            return(True)
+       
+print(prime(9))
