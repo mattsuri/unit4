4c3a769712c435a1f5a1edea2809997926929a48
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-29 08:20:41 -0400

    Updated from Brython Server: 3/29/2018 8:20:40 AM

--- warmup12.py
+++ warmup12.py
@@ -6,9 +6,9 @@
 def GCF(num1,num2):
     
     for i in range (0,num1):
-        if num1%(num2-i) == 0:
+        if num2%(num1-i) == 0 and num1%(num1-i)==0:
             return(num1 - i)
             break
         
         
-GCF(16,64)
+print(GCF(12,15))
