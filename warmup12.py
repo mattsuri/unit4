21b5c67364bb05de208a9029c50047ca667c8bc7
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-03-29 08:16:20 -0400

    Updated from Brython Server: 3/29/2018 8:16:20 AM

--- warmup12.py
+++ warmup12.py
@@ -1,3 +1,16 @@
 #Matthew Suriawinata
 #3/29/18
-#recursionDemo.py - recursive version of countdown
+#warmup12.py - GCF of two numbers
+
+
+def GCF(num1,num2):
+    if num1 < num2:
+        num1 = smaller
+        num2 = greater
+    else:
+        num2 = smaller
+        num1 = greater
+    for i in range (0,smaller):
+        if greater%(smaller-i) == 0:
+            return(smaller - i)
+            break
