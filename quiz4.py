780749718866eea92121884a377155dc9b81c6aa
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-04-02 08:30:30 -0400

    Update quiz4.py

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,32 @@
+#Matthew Suriawinata
+#4/2/18
+#quiz4.py
 
+def count(num):
+    for i in range(1,(num+1)):
+        print(i)
+        
+count(7)
+        
+
+def excitedPrint(statement):
+    print(statement.upper(), "!!!")
+
+excitedPrint("i love <3 programming")
+
+def firstLetter(word):
+    for i in word:
+        return(i)
+        break
+    
+print(firstLetter("Smeds"))
+
+def repeats(num1,num2,num3):
+    if num1 == num2 or num2 == num3 or num1 == num3:
+        return True
+    else:
+        return False
+
+
+
+print(repeats(5,6,5))
